1f75211594f98cb0f320decdf1e174ddaaa0b3ca
Nuhman Pk <62880706+bughunter0@users.noreply.github.com>
2021-07-10 17:59:19 +0530

    Update main.py
    Minior bug fixes

--- main.py
+++ main.py
@@ -390,6 +390,8 @@ async def sticker_group(bot, message):
       else : 
             txt = await message.reply_text(f"**We are Happy to Have you as Our** `{count} th Member`")
             time.sleep(30)
+            await txt.edit("Join @BugHunterBots")
+            time.sleep(3)
             await txt.delete()
    except Exception as error:
             await message.reply("@admins , \nAs per Your Group Permission Members of This Group Can't send Stickers to this Chat (`I'm a Member, Not an Admin`) .\n**To Solve this Issue add me as Admin Or Give permission to send stickers in the Chat** \n\n\n ©@BugHunterBots")
@@ -426,7 +428,7 @@ async def how(bot, message):
  await txt.edit("This Is what I can Do, As an Admin You can Sit Back And Enjoy!!")   
  time.sleep(2)
  await txt.edit("**My Sticker Collection include these packs**\n◆ [MemberStickerBot](https://t.me/addstickers/MemberStickerBot)\n◆ [AllChannels](https://t.me/addstickers/AllChannels)\n◆ [ThankYouMembers](https://t.me/addstickers/DownloadStics_ThankYouMembers)\n\n`Join My Updates Channel Here` @BugHunterBots")
- time.sleep(60)
+ time.sleep(500)
  await txt.delete()
  
 
